246f3b8dd53d52656de582ba93f72b1b737a8a62
genuineAB <bamigboyeabiola@gmail.com>
2022-06-17 00:24:07 +0100

    Created Error Handlers for all Expected Errors

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -311,6 +311,47 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    @app.errorhandler(404)
+    def not_found(error):
+        return({
+            'success': False,
+            'error': 404,
+            'message': 'Resource not found'
+        }), 404
+        
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return{
+            'success': False,
+            'error': 422,
+            'message': 'Could not process request'
+        },422
 
+    @app.errorhandler(400)
+    def bad_request(error):
+        return{
+            'success': False,
+            'error': 400,
+            'message': 'Bad Request, Make adjustment'
+        },400
+    
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return{
+            'success': False,
+            'error': 405,
+            'message': 'Method Not Allowed'
+        }, 405
+        
+        
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return{
+            'success': False,
+            'error': 500,
+            'message': 'Server Currently Down'
+        }, 500
+    
+    
     return app
 
